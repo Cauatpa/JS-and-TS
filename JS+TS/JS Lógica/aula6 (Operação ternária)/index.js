fc8fc65b4dc0ff949569ac41c1be9c10e84cb598
Cauã Thurler <101412705+Cauatpa@users.noreply.github.com>
2024-06-13 08:53:46 -0300

    Add files via upload

---
+++ JS+TS/JS Lógica/aula6 (Operação ternária)/index.js
@@ -0,0 +1,5 @@
+//Operação ternária
+
+const pontuacaoUsuario = 1500;
+const nivelUsuario = pontuacaoUsuario >= 1000? 'Usuário VIP' : 'Usuário normal';
+console.log(nivelUsuario);
---
+++ JS+TS/JS Lógica/aula7 (Objeto Date)/index.js
@@ -0,0 +1,5 @@
+// Objeto Date
+
+const data = new Date(); // Ano, Mês, Dia, Hora, Minuto, Segundo e Milesimo.
+                        // Com o Date fazio mostra a data atual.
+console.log(data.toString());
