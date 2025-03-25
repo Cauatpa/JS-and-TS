f6db52a1774f4be90fc97262872e5060421a5fce
Cauã Thurler <101412705+Cauatpa@users.noreply.github.com>
2025-03-25 17:06:38 -0300

    Add new aulas

---
+++ JS+TS/JS Tooling e ES6 Modules/aula3ImportExport/src/index.js
@@ -0,0 +1,10 @@
+// import { nome, sobrenome, idade, soma } from "./modulo1";
+import * as Meumodulo from "./modulo1";
+
+console.log(Meumodulo);
+
+// console.log(nome);
+// console.log(sobrenome);
+// console.log(idade);
+
+// console.log(soma(1, 2));
---
+++ JS+TS/JS Tooling e ES6 Modules/aula3ImportExport/src/modulo1.js
@@ -0,0 +1,9 @@
+export const nome = "Cauã";
+export const sobrenome = "Thurler";
+export const idade = 21;
+
+export function soma(x, y) {
+  return x + y;
+}
+
+// export { nome, sobrenome, idade, soma };
